891362eaf54b7e62138e2bea9ed30e4a3baa0432
Bryan Markham <muzik702@gmail.com>
2024-10-18 22:54:01 -0400

    configuring for netlify

--- .netlify/server/manifest.js
+++ .netlify/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["_redirects","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.CxlJLpAx.js","app":"_app/immutable/entry/app.BQz5wGkf.js","imports":["_app/immutable/entry/start.CxlJLpAx.js","_app/immutable/chunks/entry.DCOyz2b8.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.BQz5wGkf.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.DGhVwavX.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.CEqoory1.js","app":"_app/immutable/entry/app.D3oDtczp.js","imports":["_app/immutable/entry/start.CEqoory1.js","_app/immutable/chunks/entry.DrZb2jF2.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.D3oDtczp.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.DGhVwavX.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- build/_app/immutable/entry/start.CxlJLpAx.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DCOyz2b8.js";export{t as start};
