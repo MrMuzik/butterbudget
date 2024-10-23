360ee80f700ccb4a86cc66ddb41e6b8acb219086
Bryan Markham <muzik702@gmail.com>
2024-10-23 19:06:06 -0400

    adding logging to find error

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let error;
+    export let status;
+    console.log("Error during rendering:", error);
+</script>
+
+{#if error}
+    <h1>An error occurred: {status}</h1>
+    <p>{error.message}</p>
+{/if}
+
+<slot />
