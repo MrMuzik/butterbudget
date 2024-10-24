13946bc318bc13338e5183245d01a3ed67294a18
Bryan Markham <muzik702@gmail.com>
2024-10-24 14:06:48 -0400

    removing server and checking env vars

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,26 +1,25 @@
-export const prerender = false;
-export const ssr = true;
+// export const prerender = false;
+// export const ssr = true;
 
-// Fetch the API base URL from environment variables
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log('Befor fetch API_BASE_URL:', API_BASE_URL);
-export async function load({ fetch }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response> }) {
-  console.log('before try');
-  try {
-    const res = await fetch(`${API_BASE_URL}/api/budgets`);
+// // Fetch the API base URL from environment variables
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-    // Check if the response is OK (status 200–299)
-    if (!res.ok) {
-      console.error(`Failed to fetch budgets. Status: ${res.status}`);
-      return { budgets: [] };  // Return an empty array in case of error
-    }
-    console.log('before await', res);
-    const budgets = await res.json();
-    return { budgets };
+// export async function load({ fetch }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response> }) {
+//   try {
+//     const res = await fetch(`${API_BASE_URL}/api/budgets`);
 
-  } catch (error) {
-    console.error('Error fetching budgets:', error);
-    return { budgets: [] };  // Return an empty array in case of error
-  }
-}
+//     // Check if the response is OK (status 200–299)
+//     if (!res.ok) {
+//       console.error(`Failed to fetch budgets. Status: ${res.status}`);
+//       return { budgets: [] };  // Return an empty array in case of error
+//     }
+
+//     const budgets = await res.json();
+//     return { budgets };
+
+//   } catch (error) {
+//     console.error('Error fetching budgets:', error);
+//     return { budgets: [] };  // Return an empty array in case of error
+//   }
+// }
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    console.log('Layout component is loading...');
+    console.log('Layout component is loading...', import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_SUPABASE_URL, import.meta.env.FRONTEND_URL);
 </script>
 
 <slot />
