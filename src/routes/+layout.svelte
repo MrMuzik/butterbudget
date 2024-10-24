336ec928df2f0844b15b7c45bf3f522b83844794
Bryan Markham <muzik702@gmail.com>
2024-10-24 14:19:23 -0400

    testing env var change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    console.log('Layout component is loading...', import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_SUPABASE_URL, import.meta.env.FRONTEND_URL);
+    console.log('Layout component is loading...', import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_FRONTEND_URL);
     console.log('Need frontend URL to be set in .env file');
 </script>
 
