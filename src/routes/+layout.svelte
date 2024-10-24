c7130f95bf34f6d62922e62624537ee691da3a47
Bryan Markham <muzik702@gmail.com>
2024-10-24 14:15:34 -0400

    adding frontend env to frontend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     console.log('Layout component is loading...', import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_SUPABASE_URL, import.meta.env.FRONTEND_URL);
+    console.log('Need frontend URL to be set in .env file');
 </script>
 
 <slot />
