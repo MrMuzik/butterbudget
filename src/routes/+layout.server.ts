ffcda60e822f231f0d0f7feeb21710e5d4bf9038
Bryan Markham <muzik702@gmail.com>
2024-10-24 14:03:18 -0400

    server back with many logs

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,25 +1,26 @@
-// export const prerender = false;
-// export const ssr = true;
+export const prerender = false;
+export const ssr = true;
 
-// // Fetch the API base URL from environment variables
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// Fetch the API base URL from environment variables
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log('Befor fetch API_BASE_URL:', API_BASE_URL);
+export async function load({ fetch }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response> }) {
+  console.log('before try');
+  try {
+    const res = await fetch(`${API_BASE_URL}/api/budgets`);
 
-// export async function load({ fetch }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response> }) {
-//   try {
-//     const res = await fetch(`${API_BASE_URL}/api/budgets`);
+    // Check if the response is OK (status 200–299)
+    if (!res.ok) {
+      console.error(`Failed to fetch budgets. Status: ${res.status}`);
+      return { budgets: [] };  // Return an empty array in case of error
+    }
+    console.log('before await', res);
+    const budgets = await res.json();
+    return { budgets };
 
-//     // Check if the response is OK (status 200–299)
-//     if (!res.ok) {
-//       console.error(`Failed to fetch budgets. Status: ${res.status}`);
-//       return { budgets: [] };  // Return an empty array in case of error
-//     }
-
-//     const budgets = await res.json();
-//     return { budgets };
-
-//   } catch (error) {
-//     console.error('Error fetching budgets:', error);
-//     return { budgets: [] };  // Return an empty array in case of error
-//   }
-// }
+  } catch (error) {
+    console.error('Error fetching budgets:', error);
+    return { budgets: [] };  // Return an empty array in case of error
+  }
+}
 
